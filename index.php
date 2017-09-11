e206bfe0eb204a2e3a68b0a7d79b35f5a864963b
Nikhilesh Shankhur <nileshshankhur@gmail.com>
2017-09-11 09:02:27 +0400

    test

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 			<div class="container-fluid">
 				<div class="navbar-header page-scroll">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
+						<span class="sr-only">Toggle </span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
